b20ee1ed858999dccd4b47bce4833541bd5d2014
Unkovic D <42451399+Unko98@users.noreply.github.com>
2019-06-18 10:45:24 +0200

    Added wedding website

---
+++ scripts/index.js
@@ -0,0 +1,90 @@
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $(".navBar");
+    var $navnumber = 20;
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $navnumber);
+  });
+
+  $(document).scroll(function() {
+    var $nav = $(".navbar-brand");
+    var $navnumber = 20;
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $navnumber);
+  });
+});
+
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("section");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
+
+// Open the Modal
+function openModal() {
+  document.getele("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementsByClassName("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementsByClassName("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
